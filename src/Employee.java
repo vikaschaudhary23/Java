251e4f32856ef9e5676f1d5b1ac9be32c4c3a6b7
VIkas Chaudhary <vikaschaudhary2309@gmail.com>
2021-10-10 16:02:05 +0530

    Updated

--- src/Employee.java
+++ src/Employee.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
+import java.util.Scanner;//importing Scanner class to take input from the user
 
 public class Employee {
     public void emp(String name,int year,String next, String address){
-        System.out.println(name+" "+year+" "+address);
+        System.out.println(name+" "+year+" "+address);//printing name,year,address of the person
     }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        //objects of the Employee class
         Employee obj1 = new Employee();
         Employee obj2 = new Employee();
         Employee obj3 = new Employee();

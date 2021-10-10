a828a696d6838d807641dbcc6a263fd681b5910d
VIkas Chaudhary <vikaschaudhary2309@gmail.com>
2021-10-10 15:58:51 +0530

    Updated

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+import java.util.Scanner;//importing Scanner class to take input from the user
 public class Rectangle {
+    //instance variables
     int length;
     int breadth;
 
-    public Rectangle(int length, int breadth) {
+    public Rectangle(int length, int breadth) //parameterized constructor
+     {
         this.length = length;
         this.breadth = breadth;
     }
-    public int area(){
+    public int area()//method to return the area of rectangle
+    {
         return length*breadth;
     }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        //objects of the Rectangle class
         Rectangle obj1 = new Rectangle(s.nextInt(),s.nextInt());
         Rectangle obj2 = new Rectangle(s.nextInt(),s.nextInt());
         System.out.println(obj1.area());
